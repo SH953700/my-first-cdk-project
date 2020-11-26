4146edc5326c262b8ecb610800b9ac381192cd26
Shilpa Bindra <shilpa.bindraa@gmail.com>
2020-11-26 14:03:47 +0530

    gi

--- my_cdk_project/my_cdk_project_stack.py
+++ my_cdk_project/my_cdk_project_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import core
+from aws_cdk import core, aws_s3 as _s3
 
 
 class MyCdkProjectStack(core.Stack):
@@ -7,3 +7,10 @@ class MyCdkProjectStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="shilpabindra1",
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
+        #mybucketid is a logical identifier and not the name of the bucket
